25419d520b693b58ef4547cc40f3775f69a2801a
Elviro Rocca <elviro.rocca@facile.it>
2017-08-04 10:00:57 +0200

    added PureContstructible transformers

--- Sources/Pure.swift
+++ Sources/Pure.swift
@@ -2,3 +2,21 @@ public protocol PureConstructible {
 	associatedtype ElementType
 	init(_ value: ElementType)
 }
+
+extension PureConstructible where ElementType: PureConstructible {
+	public init(_ valueT: ElementType.ElementType) {
+		self.init(ElementType.init(valueT))
+	}
+}
+
+extension PureConstructible where ElementType: PureConstructible, ElementType.ElementType: PureConstructible {
+	public init(_ valueTT: ElementType.ElementType.ElementType) {
+		self.init(ElementType.init(ElementType.ElementType.init(valueTT)))
+	}
+}
+
+extension PureConstructible where ElementType: PureConstructible, ElementType.ElementType: PureConstructible, ElementType.ElementType.ElementType: PureConstructible {
+	public init(_ valueTTT: ElementType.ElementType.ElementType.ElementType) {
+		self.init(ElementType.init(ElementType.ElementType.init(ElementType.ElementType.ElementType.init(valueTTT))))
+	}
+}
